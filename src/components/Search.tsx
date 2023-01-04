2a26141688872f8d893b2d6e00322935d0f7aa61
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2023-01-04 11:43:11 -0300

    Adds feature to submit form by pressing Enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 import { QrCode } from 'phosphor-react';
 
@@ -11,12 +11,19 @@ type SearchProps = {
 const Search = ({ loadQrcode }: SearchProps) => {
    const [userText, setUserText] = useState('');
 
+   const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Enter') {
+         loadQrcode(userText);
+      }
+   };
+
    return (
       <div className={`${classes.search} ${'flex-col-center'}`}>
          <input
             type='text'
             placeholder='Digite um texto ou link'
             onChange={(e) => setUserText(e.target.value)}
+            onKeyDown={handleKeyDown}
          />
          <button
             className='flex-row-center'
